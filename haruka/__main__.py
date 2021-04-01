82c33057a231aee5a9b0166e70f82750d8dbfa59
matheesha official <77784129+hirushakeeth@users.noreply.github.com>
2021-04-01 22:01:48 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -22,24 +22,16 @@ from haruka.modules.connection import connected
 
 PM_START = """Hello {}, my name is {}!
 
-You know how hard it is sometimes to manage group so here is the solution for you
-
-I'm group manager bot and Anti-spam for RR Players🃏
-
-Activate Anti-spam protection in your BM by this command : 
-/antispam on 
-
-Made from the source code of [Aditya 🇮🇳](t.me/xditya)
-
-Special thanks to [this person](t.me/denver02) for helping me.
-
-Deploy a bot like me [now](www.github.com/xditya/GroupManager)
-
-Watch [video tutorial](https://www.youtube.com/watch?v=gXXFpTAk6Vo&feature=youtu.be) on deploying me.
-
-Click /help or Help button below to find out more about how to use me to my full potential.
-
- Add me to a group by clicking [here](http://t.me/tg_GroupManagerBot?startgroup=true).
+Translated from si to en.
+ Ho ... Ho .... I'm the Griffindo resident ghost 👻 ..
+ 
+ The Griffindos [❤](https://telegra.ph/file/ba3c84393f9fb34f41ea1.jpg) are the lions .. The lion cubs ..
+ 
+ I 'm with you Sir Nicholas 👻.. 
+ 
+ Welcome Lions to the Lions🇱🇰 !!!!
+ 
+ click /help to get my function 🤗
 """
 
 
@@ -157,7 +149,7 @@ def send_start(bot, update):
     first_name = update.effective_user.first_name 
     text = PM_START
 
-    keyboard = [[InlineKeyboardButton(text="🇮🇳 Language", callback_data="set_lang_")]]
+    keyboard = [[InlineKeyboardButton(text="🇦🇺 Language", callback_data="set_lang_")]]
     keyboard += [[InlineKeyboardButton(text="🛠 Reporting", callback_data="cntrl_panel_M"), 
         InlineKeyboardButton(text="❔ Help", callback_data="help_back")]]
 
